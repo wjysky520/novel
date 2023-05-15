1f40dfe43e8b4bf260cdcbf5658273b1a76925aa
wjy-talkweb <wangjunyuan@talkweb.com.cn>
2023-05-15 18:03:35 +0800

    新增基础服务

---
+++ wjysky-bizbasic-group/wjysky-system-service/src/main/java/com/wjysky/system/WjyskySystemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wjysky.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjyskySystemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjyskySystemServiceApplication.class, args);
+    }
+
+}
