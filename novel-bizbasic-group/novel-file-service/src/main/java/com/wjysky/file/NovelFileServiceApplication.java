ed41326244b99b435cfba3538c303784316599e8
wjy-talkweb <wangjunyuan@talkweb.com.cn>
2023-05-16 17:40:53 +0800

    完善框架

---
+++ novel-bizbasic-group/novel-file-service/src/main/java/com/wjysky/file/NovelFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wjysky.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelFileServiceApplication.class, args);
+    }
+
+}
